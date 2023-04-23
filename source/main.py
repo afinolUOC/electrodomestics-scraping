c330627b90f4a511bf99ba0e0c1cc9a272f04232
Arnasto <130496857+Arnasto@users.noreply.github.com>
2023-04-23 18:51:23 +0200

    Add files via upload

--- source/main.py
+++ source/main.py
@@ -1,13 +1,13 @@
 import urllib3
 import re
 from bs4 import BeautifulSoup
+import time
 import pandas as pd
+import seleniumScript
 
 
 # la funció download permet accedir a les url i ens retorna el codi html
-def download(url, user_agent="ws", num_retries=2):
-    print("Downloading", url)
-    headers = {"User-agent", user_agent}
+def download(url):
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     html = r.data.decode('utf-8')
@@ -23,42 +23,33 @@ def crawl_sitemap(url):
         print(link)
 
 
-# de la url donada retorna una llista amb els links dels productes
-# falta implementar el scroll de la pàgina
-def soup(url):
-    html = download(url)
-    soup = BeautifulSoup(html, features="html.parser")
-    a = soup.div
-    links = []
-    for card in a.find_all(class_="recomender-block-item"):
-        links.append(url + card.a["href"])
-    print(links)
-
-
-'''
-def product_scrapper(url):
-    data = dict()
-    html = download(url)
-    soup = BeautifulSoup(html, features="html.parser")
-    data["product-name"] = soup.find(class_="product-name").string
-    data["price"] = soup.find(class_="iva-included").span.string
-    data["ref"] = soup.find(class_="reference").span.string
-    data["marca"] = soup.find(class_="productPagePrices").a["title"]
-    data["val-points"] = soup.find(class_="val-points").string
-    data["val-quantity"] = soup.find(class_="val-quantity").string
-    if soup.find(class_="stock__status").span.img["src"] == "https://static.electrocosto.com/images/icons/verified.svg":
-        data["stock"] = "Disponible"
-    else:
-        data["stock"] = "No Disponible"
-    data["product-sending-value"] = soup.find(class_="sending-price-wrapper").find(class_="product-sending-value").string
-
-    print(data)
-'''
-
-
-def product_scrapper_v2(urls):
+def product_scrapper_v2(url, section):
     verified = "https://static.electrocosto.com/images/icons/verified.svg"
+
+    def url_blacklist(url):
+        html = download(url + "robots.txt")
+        banned_links = re.findall(r'Disallow:\s(.*?)\*', html)
+        return banned_links
+
+    def get_urls(url, section):
+        expanded_url = seleniumScript.expand_section(section)
+        html = download(expanded_url)
+        soup = BeautifulSoup(html, features="html.parser")
+        a = soup.div
+        links = []
+        for card in a.find_all(class_="recomender-block-item"):
+            links.append(url + section + card.a["href"])
+        return links
+
+    def check_disallowed(url_list, disallow):
+        disallow_pattern = '|'.join(disallow)
+        for item in url_list:
+            if re.search(disallow_pattern, url):
+                url_list.pop(item)
+        return url_list
+
     def scrapper_dict_setup(urllist) -> dict:
+        print("Setting up Attributes")
         # preparem les keys comunes del dict
         setup_dict = dict()
         setup_dict["product-name"] = []
@@ -70,18 +61,20 @@ def product_scrapper_v2(urls):
         setup_dict["stock"] = []
         setup_dict["product-sending-value"] = []
         # fem que busqui totes les característiques de decripció del prod i que les afegeixi al dict
-        for url in urllist:
-            html = download(url)
+        for item in urllist:
+            html = download(item)
             soup = BeautifulSoup(html, features="html.parser")
             for atr in soup.find_all(class_="product-table-atributes"):
                 elements = atr.find_all("td")
                 for i in range(0, len(elements), 2):
                     setup_dict[elements[i].string.replace("\n", "").replace(" ", "")] = []
+            time.sleep(0.5)
         return setup_dict
 
     def data_scrapper(urllist, data_dict) -> dict:
         count = 0
         for url in urllist:
+            print("Downloading", url)
             # les característiques generals apareixen a totes les URL, i les predefinim
             html = download(url)
             soup = BeautifulSoup(html, features="html.parser")
@@ -103,13 +96,14 @@ def product_scrapper_v2(urls):
                 elements = atr.find_all("td")
                 for i in range(0, len(elements), 2):
                     key = elements[i].string.replace("\n", "").replace(" ", "")
-                    value = elements[i+1].string.replace("\n", "").replace(" ", "")
+                    value = elements[i+1].string.replace("\n", "")
                     # cada element que anem a afegir mirem que tingui el nombre de entrades corresponent a les url que
                     # portem revisades, si es menor anem afegint elements NULL fins que tingui la llargada que toca
                     while len(data_dict[key]) < count:
                         data_dict[key].append("NULL")
                     data_dict[key].append(value)
             count += 1
+            time.sleep(0.5)
 
         # fem un ultim repas de que tots els elements tinguin la llargada que toca
         for key in data_dict.keys():
@@ -118,15 +112,29 @@ def product_scrapper_v2(urls):
 
         return data_dict
 
-    return data_scrapper(urls, scrapper_dict_setup(urls))
+    link_list = get_urls(url, section)
+    blacklist = url_blacklist(url)
+    clean_list = check_disallowed(link_list, blacklist)
+
+    return data_scrapper(clean_list, scrapper_dict_setup(clean_list))
 
 
-# print(download("https://www.electrocosto.com/sitemap.xml"))
+# print(re.findall(r'Disallow:\s(.*?)\*', download("https://www.electrocosto.com/robots.txt")))
 # crawl_sitemap("https://www.electrocosto.com/sitemap.xml")
-# soup("https://www.electrocosto.com/lavadoras")
+# get_urls("https://www.electrocosto.com/", "frigorificos")
 # product_scrapper("https://www.electrocosto.com/frigorificos-combis/balay-3kfe776xe-inox")
+'''
 neveras = product_scrapper_v2(["https://www.electrocosto.com/frigorificos-combis/balay-3kfe776xe-inox",
                                "https://www.electrocosto.com/frigorificos-dos-puertas/indesit-taa-5-1",
-                               "https://www.electrocosto.com/frigorificos-combis/beko-b1rche-363-w-blanco"])
+                               "https://www.electrocosto.com/frigorificos-combis/beko-b1rche-363-w-blanco",
+                               "https://www.electrocosto.com/frigorificos-americanos/kroms-kf-4p-80-ddix",
+                               "https://www.electrocosto.com/frigorificos-dos-puertas/smeg-fab30rbe5",
+                               "https://www.electrocosto.com/frigorificos-combis/bosch-kgn36vida",
+                               "https://www.electrocosto.com/frigorificos-combis/lg-gbp61swpgn",
+                               "https://www.electrocosto.com/frigorificos-combis/balay-3kfe778wi-blanco",
+                               "https://www.electrocosto.com/frigorificos-combis/winia-wrn-bv300-npt",
+                               "https://www.electrocosto.com/frigorificos-combis/balay-3kfe560wi-blanco"])
+
 neveras_data = pd.DataFrame.from_dict(neveras)
-print(neveras_data)
+neveras_data.to_csv("neveras.csv", index=False)
+'''
